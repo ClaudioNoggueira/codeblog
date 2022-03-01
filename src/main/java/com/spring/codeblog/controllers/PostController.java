0f2439b595f153202940af98b7385fa5a36a0cbb
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-03-01 13:24:02 -0300

    Create compose from and save post

--- src/main/java/com/spring/codeblog/controllers/PostController.java
+++ src/main/java/com/spring/codeblog/controllers/PostController.java
@@ -1,17 +1,23 @@
 package com.spring.codeblog.controllers;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import com.spring.codeblog.entities.Post;
 import com.spring.codeblog.services.PostService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class PostController {
@@ -33,6 +39,21 @@ public class PostController {
         Post post = service.findPostById(id);
         mv.addObject("post", post);
         return mv;
+    }
+
+    @GetMapping(value = "/compose")
+    public String getPostForm() {
+        return "postForm";
+    }
+
+    @PostMapping(value = "/compose")
+    public String savePost(@Valid Post obj, BindingResult result, RedirectAttributes attributes) {
+        if (result.hasErrors()) {
+            return "redirect:/compose";
+        }
+        obj.setDate(LocalDate.now());
+        service.savePost(obj);
+        return "redirect:/posts";
     }
 
 }
