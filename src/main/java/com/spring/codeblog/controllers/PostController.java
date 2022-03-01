b914402116f4a5c491a84ab5a1e3a8d27fb8c865
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-03-01 09:48:08 -0300

    Post controller

---
+++ src/main/java/com/spring/codeblog/controllers/PostController.java
@@ -0,0 +1,27 @@
+package com.spring.codeblog.controllers;
+
+import java.util.List;
+
+import com.spring.codeblog.entities.Post;
+import com.spring.codeblog.services.PostService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PostController {
+
+    @Autowired
+    private PostService service;
+
+    @RequestMapping(value = "/posts", method = RequestMethod.GET)
+    public ModelAndView getPosts() {
+        ModelAndView mv = new ModelAndView("posts");
+        List<Post> posts = service.findAll();
+        mv.addObject("posts", posts);
+        return mv;
+    }
+}
